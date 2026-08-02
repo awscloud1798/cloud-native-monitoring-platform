fbf39ee99aa1986a672bd13cd1c1682ced30c3f7
saran-awscloud1798 <awscloud1798@gmail.com>
2026-08-02 18:17:29 +0530

    Create variables.tf
    terraform variables

---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "location" {
+  type    = string
+  default = "centralindia"
+}
+
+variable "environment" {
+  type    = string
+  default = "prod"
+}
+
+variable "prefix" {
+  type    = string
+  default = "cnmp"
+}
+
+variable "common_tags" {
+  type = map(string)
+  default = {
+    application = "cloud-native-monitoring-platform"
+    managed_by  = "terraform"
+  }
+}
+
+variable "aks_node_count" {
+  type    = number
+  default = 3
+}
+
+variable "alert_email" {
+  type = string
+}
